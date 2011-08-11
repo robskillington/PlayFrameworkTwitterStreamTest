f0926f3e00ed1d46ef01b23be01a0c6349c4f841
Rob Skillington <rob.skillington@gmail.com>
2011-08-11 16:14:02 +1000

    A little house cleaning.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,28 +14,28 @@ public class Application extends Controller {
 
 	private static TwitterStreamer twitterStreamer = null;
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		render();
+	}
 	
 	public synchronized static void stream() {
 		if (twitterStreamer == null) {
 			twitterStreamer = new TwitterStreamer(
-				 "your consumer key",
-				 "your consumer secret",
-				 "your access token",
-				 "your access token secret"
+				"your consumer key",
+				"your consumer secret",
+				"your access token",
+				"your access token secret"
 			);
 			twitterStreamer.start();
 		}
-		
+
 		ArrayList<Status> latestStatuses = twitterStreamer.getLatestStatuses();
-		
+
 		while (latestStatuses.size() < 1) {
 			suspend(100);
 			latestStatuses = twitterStreamer.getLatestStatuses();
 		}
-	
+
 		renderJSON(latestStatuses);
 	}
 
